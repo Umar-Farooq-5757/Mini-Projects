8c663e1faa18ae6cb72d116453c3bb41309f6124
umar-farooq-5757 <aamirgel17@gmail.com>
2026-04-08 22:35:14 +0500

    Implement sound and full screen functionality

--- Chess Clock/src/App.jsx
+++ Chess Clock/src/App.jsx
@@ -1,6 +1,8 @@
-import { useState, useSyncExternalStore } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import {
+  Maximize,
+  Minimize,
   Pause,
   Play,
   RotateCw,
@@ -9,18 +11,67 @@ import {
   VolumeOff,
 } from "lucide-react";
 import Settings from "./components/Settings";
+import { createClickPlayer } from "./utils/clickSynth";
 function App() {
   const [isPaused, setIsPaused] = useState(true);
   const [isSoundEnabled, setIsSoundEnabled] = useState(true);
   const [areSettingsOpen, setAreSettingsOpen] = useState(true);
+  const [turn, setTurn] = useState('player1');
+
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const screenRef = useRef(null);
+
+  useEffect(() => {
+    const handleFullscreenChange = () => {
+      setIsFullscreen(!!document.fullscreenElement);
+    };
+    document.addEventListener("fullscreenchange", handleFullscreenChange);
+    return () => {
+      document.removeEventListener("fullscreenchange", handleFullscreenChange);
+    };
+  }, []);
+
+  const toggleFullscreen = () => {
+    if (!isFullscreen) {
+      if (screenRef.current.requestFullscreen) {
+        screenRef.current.requestFullscreen();
+      }
+    } else {
+      if (document.exitFullscreen) {
+        document.exitFullscreen();
+      }
+    }
+  };
+
+  /**
+   * Playing click sound
+   */
+  const playerRef = useRef(null);
+  useEffect(() => {
+    playerRef.current = createClickPlayer();
+    return () => {
+      if (playerRef.current) {
+        playerRef.current.dispose();
+      }
+    };
+  }, []);
+  const handleClick = () => {
+    if (playerRef.current) {
+      playerRef.current.play({ 
+        type: 'noise', 
+        pitch: 1500, 
+        decay: 0.05 
+      });
+    }
+  };
   return (
-    <main className="flex min-h-screen flex-col select-none">
+    <main ref={screenRef} className="flex min-h-screen flex-col select-none">
       {/* Player 1 time */}
-      <div className="bg-teal-500 grow text-white font-semibold text-9xl text flex justify-center items-center">
+      <div onClick={()=>{setTurn('player2');if(turn=='player1'){handleClick()}}} className={`${turn=='player1'?'bg-teal-500':'bg-[#555]'} grow text-white font-semibold text-9xl text flex justify-center items-center`}>
         2:57
       </div>
       {/* Controls */}
-      <div className="bg-black/80 text-white py-2 px-2 flex items-center justify-around">
+      <div className="bg-[#333] text-white py-2 px-2 flex items-center justify-around">
         <div onClick={() => setIsPaused(!isPaused)}>
           {isPaused ? (
             <Play className="size-5" />
@@ -41,9 +92,18 @@ function App() {
             <VolumeOff className="size-5" />
           )}
         </div>
+        <div>
+          <button onClick={toggleFullscreen}>
+            {isFullscreen ? (
+              <Minimize className="size-5" />
+            ) : (
+              <Maximize className="size-5" />
+            )}
+          </button>
+        </div>
       </div>
       {/* Player 2 time */}
-      <div className="bg-teal-500 grow text-white font-semibold text-9xl text flex justify-center items-center">
+      <div onClick={()=>{setTurn('player1');if(turn=='player2'){handleClick()}}} className={`${turn=='player2'?'bg-teal-500':'bg-[#555]'} grow text-white font-semibold text-9xl text flex justify-center items-center`}>
         2:57
       </div>
       <Settings
