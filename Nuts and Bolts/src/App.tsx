cfa95e0cd8a6d594fb735e9551da231ebcfa8c20
umar-farooq-5757 <aamirgel17@gmail.com>
2026-06-14 14:04:52 +0500

    Nuts and bolts: screw and unscrew nuts

--- Nuts and Bolts/src/App.tsx
+++ Nuts and Bolts/src/App.tsx
@@ -1,122 +1,106 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [currentNut, setCurrentNut] = useState<string>("");
+  const [nutRemovedFrom, setNutRemovedFrom] = useState<number>(-1);
+  // const [boltsArray, setBoltsArray] = useState<string[][]>([
+  //   ["green", "green", "blue"],
+  //   ["red", "blue", "blue"],
+  //   ["red", "red", "yellow"],
+  //   ["green", "yellow", "yellow"],
+  //   [],
+  //   [],
+  // ]);
+  const [boltsArray, setBoltsArray] = useState<string[][]>([
+    ["blue", "green", "green"],
+    ["blue", "blue", "red"],
+    ["yellow", "red", "red"],
+    ["yellow", "yellow", "green"],
+    [],
+    [],
+  ]);
+  const unscrew = (index: number) => {
+    let clickedBolt = boltsArray[index];
+    if (clickedBolt.length !== 0) {
+      setCurrentNut(clickedBolt.at(-1)??"");
+      setNutRemovedFrom(index)
+      setBoltsArray((prev) =>
+        prev.map((row, rowIdx) => {
+          if (index !== rowIdx) {
+            return row;
+          }
+          return row.slice(0, -1);
+        }),
+      );
+    }
+  };
+  const screw = (index: number) => {
+    let clickedArr = boltsArray[index];
+    if (clickedArr.length === 0) {
+      setBoltsArray((prev) =>
+        prev.map((arr, idx) => {
+          if (index !== idx) {
+            return arr;
+          }
+          let result = [...arr, currentNut];
+          return result;
+        }),
+      );
+    } else if(clickedArr.length<=3){
+      if (clickedArr.at(-1) === currentNut) {
+        setBoltsArray((prev) =>
+          prev.map((arr, idx) => {
+            if (index !== idx) {
+              return arr;
+            }
+            let result = [...arr, currentNut];
+            return result;
+          }),
+        );
+      } else {
+        screw(nutRemovedFrom)
+        return;
+      }
+    }else{
+      screw(nutRemovedFrom)
+    }
+    setCurrentNut("");
+  };
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
+      {currentNut !== "" && (
+        <div className="fixed text-white left-1/2 -translate-x-1/2">
+          <p>Current nut</p>
+          <div
+            style={{ backgroundColor: currentNut }}
+            className="w-20 h-10 border border-white"></div>
         </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      )}
+      <div className="flex justify-between items-center px-1 sm:px-5 md:px-20 bg-gray-700 min-h-screen">
+        {boltsArray.map((arr, index) => {
+          return (
+            <div
+              onClick={() =>
+                currentNut === "" ? unscrew(index) : screw(index)
+              }
+              key={index}
+              className="bg-gray-500 h-40 w-8 sm:w-16 flex flex-col-reverse items-center justify-start gap-1">
+              {/* nut */}
+              {arr.map((color, idx) => {
+                return (
+                  <div
+                    key={idx}
+                    className="w-full h-1/5 border border-white"
+                    style={{ backgroundColor: color }}></div>
+                );
+              })}
+            </div>
+          );
+        })}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
